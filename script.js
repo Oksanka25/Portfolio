82a3f734425a3f2f64a34609e6dd6920544b8c61
Oksanka25 <chekoksa25@gmail.com>
2022-08-01 12:06:12 -0400

    Working on contact form

---
+++ script.js
@@ -0,0 +1,13 @@
+// function sendEmail() {
+//     Email.send({
+//         Host: "smtp.gmail.com",
+//         Username: "",
+//         Password: "",
+//         To: 'chekoksa25@gmail',
+//         From: document.getElementById("email").value,
+//         Subject: "New Contact Form Inquiry",
+//         Body: "And this is the body"
+//     }).then(
+//         message => alert(message)
+//     );
+// }
