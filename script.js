38d113d742a33fb126a137ab8aa0a1ae9ac1c797
Oksanka25 <chekoksa25@gmail.com>
2022-08-02 14:33:03 -0400

    Fixed contact form

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function sendEmail() {
     Email.send({
         SecureToken: "729ab0ce-6313-4c14-8e07-a5b53ee12120",
         To: 'oksanaott302@gmail.com',
-        From: document.getElementById("email").value,
-
+        // From: document.getElementById("email").value,
+        From: "chekoksa25@gmail.com",
         Subject: "New Contact Form Inquiry",
         Body: "Name:" + document.getElementById("name").value +
             "<br> Email:" + document.getElementById("email").value +
@@ -13,3 +13,16 @@ function sendEmail() {
         message => alert(message)
     );
 }
+
+// const nameValue = document.getElementById("name").value;
+// const emailValue = document.getElementById("email").value;
+// const phoneValue = document.getElementById("phone").value;
+// const messageValue = document.getElementById("message").value;
+
+
+// const sendEmailLink = document.getElementById("email");
+
+// sendEmailLink.addEventListener("click", () => {
+//     sendEmailLink.href = `mailto:chekoksa25@gmail.com?subject=Inquiry&body=${messageValue}`;
+//     sendEmailLink.click();
+// })
