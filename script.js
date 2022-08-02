f6504abb086a56f3ac0ed79cea056165061a54d4
Oksanka25 <chekoksa25@gmail.com>
2022-08-01 20:39:24 -0400

    Fixing contact form

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// function sendEmail() {
-//     Email.send({
-//         Host: "smtp.gmail.com",
-//         Username: "",
-//         Password: "",
-//         To: 'chekoksa25@gmail',
-//         From: document.getElementById("email").value,
-//         Subject: "New Contact Form Inquiry",
-//         Body: "And this is the body"
-//     }).then(
-//         message => alert(message)
-//     );
-// }
+function sendEmail() {
+    Email.send({
+        SecureToken: "729ab0ce-6313-4c14-8e07-a5b53ee12120",
+        To: 'oksanaott302@gmail.com',
+        From: document.getElementById("email").value,
+
+        Subject: "New Contact Form Inquiry",
+        Body: "Name:" + document.getElementById("name").value +
+            "<br> Email:" + document.getElementById("email").value +
+            "<br> Phone:" + document.getElementById("phone").value +
+            "<br> Message:" + document.getElementById("message").value
+    }).then(
+        message => alert(message)
+    );
+}
